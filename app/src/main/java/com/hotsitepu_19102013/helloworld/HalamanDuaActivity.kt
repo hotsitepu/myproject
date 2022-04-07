520f4164d8adf57fba8a9a96430e972d3014d17a
hotsitepu <googoplexidn@gmail.com>
2022-04-07 23:47:15 +0700

    tugas 3

---
+++ app/src/main/java/com/hotsitepu_19102013/helloworld/HalamanDuaActivity.kt
@@ -0,0 +1,11 @@
+package com.hotsitepu_19102013.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanDuaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_dua)
+    }
+}
